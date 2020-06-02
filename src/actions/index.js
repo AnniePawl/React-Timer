bf5e5bbdc1946788d7d3dfbbf14e061e36d74172
Anna Pawl <annampawl@gmail.com>
2020-06-02 18:51:02 -0400

    added timer actions

---
+++ src/actions/index.js
@@ -0,0 +1,37 @@
+// All actions defined here 
+// Actions descibe how data can be modified
+// Each action should be defined as const w/ string value 
+// Each action needs action creator 
+
+
+// Add Timer 
+// Adds new timer object to array of timers
+export const addTimer = (name) => {
+  return {
+    type: NEW_TIMER, 
+    payload: {name}
+  }
+}
+
+// Toggle Timer 
+// Starts and stops timer (required index of timer)
+export const toggleTimer = (index) => {
+  return {
+    type: TOGGLE_TIMER, 
+    payload: {index}
+  }
+}
+
+// Select Timer 
+// Selects a timer and displays details about it (requires index of timer)
+export const selectTimer = (index) => {
+  return {
+    type: SELECT_TIMER, 
+    payload: {index}
+  }
+}
+
+// Export actions so other modules can access them 
+export const NEW_TIMER = "NEW_TIMER"
+export const TOGGLE_TIMER = "TOGGLE_TIMER"
+export const SELECT_TIMER = "SELECT_TIMER"
